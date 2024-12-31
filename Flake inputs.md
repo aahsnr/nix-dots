4feb97531334f0a5df6007f1d07292c03475b31a
zielOS <ahsanur041@gmail.com>
2024-12-31 11:43:37 +0600

    updated flake.nix inputs

---
+++ Flake inputs.md
@@ -0,0 +1,20 @@
+## Essential Flakes
+
+- [ ] dev-shell
+- [ ] chaotic
+- [ ] flake-utils
+- [ ] home-manager
+- [ ] nixpkgs
+- [ ] systems
+
+## Packages Flakes
+
+- [ ] ags
+- [ ] anyrun
+- [ ] emacs-overlay
+- [ ] hyprland
+- [ ] hyprland-contrib
+- [ ] hyprland-plugins
+- [ ] hyprlock
+- [ ] hypridle
+- [ ] 
--- flake.nix
+++ flake.nix
@@ -2,59 +2,103 @@
   description = "My NixOS configuration";
 
   inputs = {
-    ags.url = "github:Aylur/ags";
 
+    ##-- Essential Flakes
+    devshell.url = "github:numtide/devshell";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default-linux";
+
+    ##-- Packages Flakes
+    ags.url = "github:Aylur/ags";
     anyrun = {
       url = "github:Kirottu/anyrun";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-
-    devshell.url = "github:numtide/devshell";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     hyprland.url = "github:hyprwm/Hyprland/";
-
+    hyprland-contrib = {
+      url = "github:hyprwm/contrib";
+      inputs.nixpkgs.follows = "hyprland/nixpkgs";
+    };
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
     };
-
+    hypridle = {
+      url = "github:hyprwm/hypridle";
+      inputs = {
+        hyprlang.follows = "hyprland/hyprlang";
+        hyprutils.follows = "hyprland/hyprutils";
+        nixpkgs.follows = "hyprland/nixpkgs";
+        systems.follows = "hyprland/systems";
+      };
+    };
+    hyprlock = {
+      url = "github:hyprwm/hyprlock";
+      inputs = {
+        hyprlang.follows = "hyprland/hyprlang";
+        hyprutils.follows = "hyprland/hyprutils";
+        nixpkgs.follows = "hyprland/nixpkgs";
+        systems.follows = "hyprland/systems";
+      };
+    };
+    hyprpaper = {
+      url = "github:hyprwm/hyprpaper";
+      inputs = {
+        hyprlang.follows = "hyprland/hyprlang";
+        hyprutils.follows = "hyprland/hyprutils";
+        nixpkgs.follows = "hyprland/nixpkgs";
+        systems.follows = "hyprland/systems";
+      };
+    };
     Hyprspace = {
-      url = github:KZDKM/Hyprspace;
+      url = "github:KZDKM/Hyprspace";
       inputs.hyprland.follows = "hyprland";
     };
-
-    hypridle.url = "github:hyprwm/hypridle";
-
-    hyprlock.url = "github:hyprwm/hyprlock";
-
-    hyprland-contrib.url = "github:hyprwm/contrib";
-
-    matugen.url = "github:InioX/matugen";
+    hy3 = {
+      url = "github:outfoxxed/hy3"; 
+      inputs.hyprland.follows = "hyprland";
+    };
 
     nil = {
       url = "github:oxalica/nil";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.rust-overlay.follows = "rust-overlay";
     };
-
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
-
+    nix-doom-emacs-unstraightened = {
+      url = "github:marienz/nix-doom-emacs-unstraightened";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs-wayland = {
+      url = "github:nix-community/nixpkgs-wayland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs-f2k.url = "github:fortuneteller2k/nixpkgs-f2k";
-
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixos-hardware.url = "github:nixos/nixos-hardware";
-
     nix-gaming.url = "github:fufexan/nix-gaming";
-
     nur.url = "github:nix-community/NUR";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    stm.url = "github:Aylur/stm";
 
     pyprland = {
       url = "github:hyprland-community/pyprland";
@@ -63,35 +107,22 @@
       
     xdg-portal-hyprland.url = "github:hyprwm/xdg-desktop-portal-hyprland";
 
-    stm.url = "github:Aylur/stm";
- 
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
- 
+
   };
 
-  outputs = { 
-    self, 
-    nixpkgs, 
-    chaotic, 
-    home-manager, 
-    anyrun, 
-    ...
+  outputs = { anyrun, home-manager, nixpkgs, self, yazi,  ... }@inputs: 
+    let
+      system = "x86_64-linux";
+      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      nixosConfigurations = import ./hosts inputs;
 
-    } @ inputs: let
-    system = "x86_64-linux";
-    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    nixosConfigurations = import ./hosts inputs;
-
-    packages.${system} = {
-      #catppuccin-folders = pkgs.callPackage ./pkgs/catppuccin-folders.nix {};
-      #catppuccin-gtk = pkgs.callPackage ./pkgs/catppuccin-gtk.nix {};
-      #catppuccin-cursors = pkgs.callPackage ./pkgs/catppuccin-cursors.nix {};
-      # onlyoffice-deb = pkgs.callPackage ./pkgs/onlyoffice-bin.nix {};
-      # insync-deb = pkgs.callPackage ./pkgs/insync-deb.nix {};
+      packages.${system} = {
+        #catppuccin-folders = pkgs.callPackage ./pkgs/catppuccin-folders.nix {};
+        #catppuccin-gtk = pkgs.callPackage ./pkgs/catppuccin-gtk.nix {};
+        #catppuccin-cursors = pkgs.callPackage ./pkgs/catppuccin-cursors.nix {};
+        #onlyoffice-deb = pkgs.callPackage ./pkgs/onlyoffice-bin.nix {};
+        #insync-deb = pkgs.callPackage ./pkgs/insync-deb.nix {};
     };
   };
 }
